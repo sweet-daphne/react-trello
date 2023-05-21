ee404d2d1640dd1db547c682647d74f284db5b1c
suelrin <dancerelrin@gmail.com>
2023-05-22 06:51:22 +0900

    creating tasks

--- src/App.tsx
+++ src/App.tsx
@@ -24,13 +24,14 @@ const Boards = styled.div`
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
   const onDragEnd = (info: DropResult) => {
-    const { destination, draggableId, source } = info;
+    const { destination, source } = info;
     if (!destination) return;
     if (destination?.droppableId === source.droppableId) {
       setToDos((allBoards) => {
         const boardCopy = [...allBoards[source.droppableId]];
+        const taskObj = boardCopy[source.index];
         boardCopy.splice(source.index, 1);
-        boardCopy.splice(destination?.index, 0, draggableId);
+        boardCopy.splice(destination?.index, 0, taskObj);
         return {
           ...allBoards,
           [source.droppableId]: boardCopy,
@@ -40,9 +41,10 @@ function App() {
     if (destination.droppableId !== source.droppableId) {
       setToDos((allBoard) => {
         const sourceBoard = [...allBoard[source.droppableId]];
+        const taskObj = sourceBoard[source.index];
         const destinationBoard = [...allBoard[destination.droppableId]];
         sourceBoard.splice(source.index, 1);
-        destinationBoard.splice(destination?.index, 0, draggableId);
+        destinationBoard.splice(destination?.index, 0, taskObj);
         return {
           ...allBoard,
           [source.droppableId]: sourceBoard,
